ef1674262003136b2eac9b44f61c263e54432e58
shenxiaoqing <18137702933@163.com>
2017-12-12 15:46:47 +0800

    学习第一课，以后要继续坚持

---
+++ event.js
@@ -0,0 +1,16 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+var connectHandler = function connected() {
+    console.log('连接成功。');
+    eventEmitter.emit('data_received');
+}
+
+eventEmitter.on('connection', connectHandler);
+
+eventEmitter.on('data_received', function() {
+    console.log('新数据接受成功');
+})
+
+eventEmitter.emit('connection');
+
+console.log('程序执行完毕。');
