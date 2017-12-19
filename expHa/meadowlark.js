5964af55bdffc09806c1bd18e1ee6774764d72d6
shenxiaoqing <18137702933@163.com>
2017-12-19 21:22:58 +0800

    添加测试代码，哈哈mocha

--- expHa/meadowlark.js
+++ expHa/meadowlark.js
@@ -18,6 +18,11 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
@@ -31,7 +36,10 @@ app.get('/about', function(req, res){
 	//res.send('About Meadowlark Travel');
 	// var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 	var fortune = require('./lib/fortune.js');
-	res.render('about', { fortune: fortune.getFortune() });
+	res.render('about', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
 	// res.render('about', { fortune: randomFortune });
 });
 
---
+++ expHa/public/qa/tests-global.js
@@ -0,0 +1,5 @@
+suite('Global Tests', function(){
+	test('Page has a valid title', function(){
+		assert(document.title && document.title.match(/\S/) && document.title.toUpperCase() !== 'TODO');
+	});
+});
